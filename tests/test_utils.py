de9d5a3a0a136c0ffb4fa74ea52df42ffd92bf5c
Liudmila <leroshkina@mail.ru>
2024-08-02 12:32:45 +0300

    "relative path"

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,12 +1,12 @@
 from unittest.mock import patch
-
+import os
 from src.utils import create_objects_from_json, get_json_data
 
 
 @patch('json.load')
 def test_get_json_data(mock_json, json_data):
     mock_json.return_value = json_data
-    assert get_json_data(r'C:\Users\liudo\PycharmProjects\skystore_eroshkinalv\data\products.json') == json_data
+    assert get_json_data(os.path.abspath(r'.\data\products.json')) == json_data
 
 
 def test_create_objects_from_json(json_data):
